b5b86875c5b0cf07faf08f8a517620a5829e3dc7
MDAbuSabbir <mdabusabbir356155@gmail.com>
2025-10-14 20:52:31 +0600

    Firebase Configaration Starts

---
+++ src/Firebase Configaration/Firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCgsNXaRB7iG_T6hGO0qiDbzgr6B4GBfSE",
+  authDomain: "react-auth-integration-8b679.firebaseapp.com",
+  projectId: "react-auth-integration-8b679",
+  storageBucket: "react-auth-integration-8b679.firebasestorage.app",
+  messagingSenderId: "97762101067",
+  appId: "1:97762101067:web:40f50c943134f9b521cace"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
